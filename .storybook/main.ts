157565331746020679321c9b9d362323f38ea8fd
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-20 15:32:46 +0300

    feat(storybook): add alias support

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
@@ -14,6 +15,16 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  async webpackFinal(config) {
+    if (config?.resolve?.alias) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@/assets": path.resolve(__dirname, "../src/assets"),
+        "@/components": path.resolve(__dirname, "../src/components/"),
+      };
+    }
+    return config;
   },
 };
 export default config;
