c80d6a675f1edb77a59f3745ed5f26322e824c9e
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-22 15:48:39 +0300

    fix(storybook): add alias for constants

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -22,6 +22,7 @@ const config: StorybookConfig = {
         ...config.resolve.alias,
         "@/assets": path.resolve(__dirname, "../src/assets"),
         "@/components": path.resolve(__dirname, "../src/components/"),
+        "@/constants": path.resolve(__dirname, "../src/components/"),
       };
     }
     return config;
