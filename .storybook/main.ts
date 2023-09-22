c96f7ea40b6b6df5745f96def9ea2b923172b565
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-22 15:57:16 +0300

    fix(storybook): correct path for constants

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -22,7 +22,7 @@ const config: StorybookConfig = {
         ...config.resolve.alias,
         "@/assets": path.resolve(__dirname, "../src/assets"),
         "@/components": path.resolve(__dirname, "../src/components/"),
-        "@/constants": path.resolve(__dirname, "../src/components/"),
+        "@/constants": path.resolve(__dirname, "../src/constants/"),
       };
     }
     return config;
