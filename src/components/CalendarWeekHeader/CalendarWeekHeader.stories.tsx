5e219b4d9aed6da1916f6f051016b224be0c5465
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-25 17:54:57 +0300

    feat(components): update `CalendarWeekHeader`
    Add property to control sunday/monday position
    Add unit tests

--- src/components/CalendarWeekHeader/CalendarWeekHeader.stories.tsx
+++ src/components/CalendarWeekHeader/CalendarWeekHeader.stories.tsx
@@ -5,9 +5,22 @@ import CalendarWeekHeader from "./CalendarWeekHeader";
 const meta: Meta<typeof CalendarWeekHeader> = {
   title: "Component/CalendarWeekHeader",
   component: CalendarWeekHeader,
+  argTypes: {
+    isSundayFirst: {
+      name: "Is Sunday First",
+      description: "Controls is Sunday first day of week or not",
+      defaultValue: true,
+      type: "boolean",
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof CalendarWeekHeader>;
 
-export const Primary: Story = {};
+export const SundayFirst: Story = {};
+export const MondayFirst: Story = {
+  args: {
+    isSundayFirst: false,
+  },
+};
--- src/components/CalendarWeekHeader/CalendarWeekHeader.tsx
+++ src/components/CalendarWeekHeader/CalendarWeekHeader.tsx
@@ -1,14 +1,21 @@
-import React, { memo } from "react";
+import React, { memo, useMemo } from "react";
 
 import { WEEKS_DATA } from "@/constants/weekData";
 
 import CalendarButton from "../CalendarButton";
 import { Wrapper } from "./CalendarWeekHeader.styled";
+import { CalendarWeekHeaderProps } from "./interfaces";
 
-const CalendarWeekHeader = memo(function CalendarWeekHeader() {
+const CalendarWeekHeader = memo(function CalendarWeekHeader({
+  isSundayFirst = true,
+}: CalendarWeekHeaderProps) {
+  const weeksArray = useMemo(() => {
+    return isSundayFirst ? WEEKS_DATA : [...WEEKS_DATA.slice(1), WEEKS_DATA[0]];
+  }, [isSundayFirst]);
+
   return (
     <Wrapper>
-      {WEEKS_DATA.map(({ id, title }) => (
+      {weeksArray.map(({ id, title }) => (
         <CalendarButton key={id} title={title} />
       ))}
     </Wrapper>
