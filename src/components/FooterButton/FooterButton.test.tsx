802e183daa9f2d883244ceab5c922d0e781a9a8a
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-25 19:36:19 +0300

    refactor(components): update `FooterButton`
    Add property for `onClick` (Optional)
    Refactor tests for button

--- src/components/FooterButton/FooterButton.test.tsx
+++ src/components/FooterButton/FooterButton.test.tsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 
 import FooterButton from "./FooterButton";
 
-describe("Button", () => {
-  test("renders the FooterButton component", () => {
-    render(<FooterButton title="Click me!" />);
+describe("FooterButton", () => {
+  it("should render the button with the correct title", () => {
+    const { getByText } = render(<FooterButton title="Click me!" />);
+    const button = getByText("Click me!");
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it("should call the onClick function when the button is clicked", () => {
+    const onClick = jest.fn();
+    const { getByText } = render(
+      <FooterButton title="Click me!" onClick={onClick} />,
+    );
+    const button = getByText("Click me!");
+
+    fireEvent.click(button);
+
+    expect(onClick).toHaveBeenCalled();
   });
 });
--- src/components/FooterButton/FooterButton.tsx
+++ src/components/FooterButton/FooterButton.tsx
@@ -3,8 +3,11 @@ import React, { memo } from "react";
 import { Button } from "./FooterButton.styled";
 import { FooterButtonProps } from "./interfaces";
 
-const FooterButton = memo(function FooterButton({ title }: FooterButtonProps) {
-  return <Button>{title}</Button>;
+const FooterButton = memo(function FooterButton({
+  title,
+  onClick,
+}: FooterButtonProps) {
+  return <Button onClick={onClick}>{title}</Button>;
 });
 
 export default FooterButton;
