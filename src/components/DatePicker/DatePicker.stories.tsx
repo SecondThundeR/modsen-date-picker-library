d399127a63ccc8e7f2a5ac590b97a6b999e8f0bc
SecondThundeR <awayfromgalaxy@gmail.com>
2023-09-27 16:13:20 +0300

    feat(components): update `DatePicker` stories
    Add examples with HOCs

--- src/components/DatePicker/DatePicker.stories.tsx
+++ src/components/DatePicker/DatePicker.stories.tsx
@@ -1,4 +1,9 @@
+import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
+
+import { withHolidays } from "@/hocs/withHolidays";
+import { withMondayFirst } from "@/hocs/withMondayFirst";
+import { withWeekends } from "@/hocs/withWeekends";
 
 import DatePicker from "./DatePicker";
 
@@ -43,4 +48,24 @@ export const Primary: Story = {
   args: {
     holidays: { [new Date().getMonth() + 1]: [3, 6, 8, 10, 23, 30] },
   },
+};
+
+const DatePickerWithWeekends = withWeekends(DatePicker);
+
+export const WithWeekendsHOC: Story = {
+  render: () => <DatePickerWithWeekends />,
+};
+
+const DatePickerWithHolidays = withHolidays(DatePicker, {
+  [new Date().getMonth() + 1]: [2, 10, 18, 19, 20, 30],
+});
+
+export const WithHolidaysHOC: Story = {
+  render: () => <DatePickerWithHolidays />,
+};
+
+const DatePickerWithMondayFirst = withMondayFirst(DatePicker);
+
+export const WithMondayFirstHOC: Story = {
+  render: () => <DatePickerWithMondayFirst />,
 };
