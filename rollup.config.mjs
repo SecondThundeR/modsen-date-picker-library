86c5dd89d4695abf32ff5e09f7e3732912c5122a
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-02 15:22:03 +0300

    chore: update build related files
    Add scripts for building and generating types
    Add `styled-components` as `peerDependencies`
    Update GH Action workflow to run build command
    Migrate to `dts-bundle-generator` as `rollup-plugin-dts` failed to generate bundled types

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,10 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import dts from "rollup-plugin-dts";
 
 import packageJson from "./package.json" assert { type: "json" };
-import tsconfigJson from "./tsconfig.json" assert { type: "json" };
 
 /** @type {import('rollup').RollupOptions}*/
 export default [
@@ -32,15 +30,5 @@ export default [
       typescript({ tsconfig: "./tsconfig.json" }),
       terser(),
     ],
-  },
-  {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts({
-      compilerOptions: {
-        baseUrl: tsconfigJson.compilerOptions.baseUrl,
-        paths: tsconfigJson.compilerOptions.paths,
-      },
-    }),],
   },
 ];
